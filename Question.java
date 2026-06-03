a99ef657718a8be63c34ba216e3136487b4219fa
Ilupeju Daniel <danielilupeju12@gmail.com>
2026-06-03 10:27:04 +0100

    Java Output

---
+++ Question.java
@@ -0,0 +1,12 @@
+public class Question{
+	public static void main(String[] args){
+		System.out.println("pls, i do not  understand the use of the inbuilt words, static and void");
+		System.out.println("Learning java is fun");
+		System.out.print("Here, i, am using the print method.");
+		System.out.print(" This text will be appended meaning add.");
+		System.out.printf(" %nI am %d years old.%n" ,25);
+		System.out.printf("My account balance is %c%,.2f%n",'$', 7654.999867);
+		System.out.printf("%s is a boy%n","Benjamin");
+		System.out.printf("Do you love java? %b%n" ,true);System.out.printf("The fuel has been increased by %%%n",80);
+	}
+}
